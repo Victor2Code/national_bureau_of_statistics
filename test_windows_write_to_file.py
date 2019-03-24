cd9212196df3141a8b5a91665dae78a72e9013a5
Victor2Code <fhx0704@gmail.com>
2019-03-24 14:10:14 +0800

    test windows environment only

---
+++ test_windows_write_to_file.py
@@ -0,0 +1,8 @@
+#usr/bin/env python
+#python3
+
+with open('temp.txt','w',newline='\n') as f:
+    f.write('123'+' '+'456'+'\n')
+    f.write('123'+' '+'456'+'\n')
+    f.write('123'+' '+'456'+'\n')
+    f.write('end')
